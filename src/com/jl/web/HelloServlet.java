6cc010a832f109bb5e79c4c5358f9a884f92ebae
xiaojia <2026150772@qq.com>
2018-12-03 16:42:43 +0800

    init

---
+++ src/com/jl/web/HelloServlet.java
@@ -0,0 +1,13 @@
+package com.jl.web;
+
+import java.io.IOException;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.getWriter().write("hello jl");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+}
